ba94d5b441104265a0c03081f3175565c03dda8f
Asliddinbek Azizovich <46614434+abdivasiyev@users.noreply.github.com>
2024-04-07 20:26:35 +0500

    refresh token api

--- docs/user/sidebar.js
+++ docs/user/sidebar.js
@@ -1,1 +1,1 @@
-module.exports = [{"type":"doc","id":"user/user-service"},{"type":"category","label":"Auth","items":[{"type":"doc","id":"user/auth-by-phone","label":"Auth by phone","className":"api-method post"}]}];
+module.exports = [{"type":"doc","id":"user/user-service"},{"type":"category","label":"Auth","items":[{"type":"doc","id":"user/auth-by-phone","label":"Auth by phone","className":"api-method post"},{"type":"doc","id":"user/refresh-auth-token","label":"Refresh auth token","className":"api-method post"}]}];
